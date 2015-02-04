7de0c43994e60b91af38d0a73be9a7b4ffd84219
tunnckoCore <mameto_100@mail.bg>
2015-02-04 02:32:52 +0200

    dev branch

--- index.js
+++ index.js
@@ -7,41 +7,147 @@
 
 'use strict';
 
-var assert = require('assert')
-var chalk = require('chalk');
-var logSymbols = require('log-symbols');
+var assert = require('assert');
+var globby = require('globby');
+// var Ware = require('ware'); // ???
+var format = require('util').format;
+var errs = require('handle-errors')('mukla');
 
-var mukla = module.exports = function mukla(title, fn) {
-  var promise = Promise.resolve(1);
-  if (!fn && typeof fn !== 'function') {
-    return reporter(title, promise);
+module.exports = Mukla;
+
+function Mukla(heading, options) {
+  if (!(this instanceof Mukla)) {
+    return new Mukla(heading, opts);
   }
-  return promise.then(fn.bind(this)).then(function() {
-    console.log(logSymbols.success, chalk.grey(title));
-  })
-  .catch(function(err) {
-    console.log(logSymbols.error, chalk.red(title));
-  });
-}
+  this.id = 0;
+  this.title = 'unnamed test ' + this.id;
+  this.heading = heading || 'mukla testing:';
 
-function reporter(title, promise) {
-  var o = {};
-  Object.keys(assert).forEach(function(method) {
-    o[method] = function(actual, expected) {
-      return promise.then(function(res) {
-        assert[method].apply(assert[method], [actual, expected, title]);
-        console.log('  %s %s', logSymbols.success, chalk.grey(title));
-        return res;
-      })
-      .catch(function(err) {
-        console.log('  %s %s', logSymbols.error, chalk.grey(title));
-        console.log('  ---')
-        console.log(chalk.red('  actual: %s'), JSON.stringify(err.actual, 0,0))
-        console.log(chalk.green('  expected: %s'), JSON.stringify(err.expected, 0,0))
-        console.log('  operator: %s', err.operator)
-        console.log('  ---')
-      });
+  // this.ware = new Ware();
+  this.store = [];
+  this.options = options || {};
+  this.current = false;
+  this.isShould = false;
+  this.isThen = false;
+  this.isAssertion = false;
+
+  this.defaultOptions();
+};
+
+/**
+ * > Initialize default options
+ *
+ * @api private
+ */
+Mukla.prototype.defaultOptions = function defaultOptions() {
+  var self = this;
+  this.options.runner = new EventEmitter();
+  this.options.reporter = new Reporter(this.options.runner, this.options);
+  this.options.files = [];
+  globby(['./test.js', './*.test.js', './test'], function(err, files) {
+    if (!err) {
+      self.options.files.concat(files);
     }
-  });
-  return o;
+    throw err;
+  })
+};
+
+/**
+ * > Add a `plugin` to the middleware stack.
+ *
+ * @param {Function|Array|Ware} `plugin`
+ * @return {Mukla}
+ * @api public
+ */
+// ???
+// Mukla.prototype.use = function use(plugin){
+//   this.ware.use(plugin);
+//   return this;
+// };
+
+/**
+ * > Run all test suites or specify their order of running.
+ *
+ * @param {Object} `files`
+ * @return {Object}
+ * @api public
+ */
+
+Mukla.prototype.run = function run() {};
+
+/**
+ * > Add test suite to the stack
+ *
+ * @param  {String}   `<title>`
+ * @param  {Function} `[fn]`
+ * @param  {Number}   `[id]`
+ * @return {Mukla}
+ * @api public
+ */
+Mukla.prototype.describe =
+Mukla.prototype.when =
+Mukla.prototype.should = function should(title, fn, id) {
+  if (!title) {
+    errs.error('`.should` method expect at least 1 argument - string');
+  }
+  if (typeof fn === 'number') {
+    id = fn;
+    fn = undefined;
+  }
+
+  id = this.id = id ? id : this.store.length + 1;
+
+  if (this.store[id].id === id) {
+    errs.error(format('this `%s` test id already exist', id);
+  }
+
+  this.current = {
+    suite: true,
+    test: fn ? fn : false,
+    assertion: false,
+    title: title,
+    fn: fn,
+    id: id
+  };
+  if (fn) {
+    this.store.push(this.current);
+    this.current = false;
+  }
+  return this;
+};
+
+/**
+ * > Add test to the suite
+ *
+ * @param  {String}   `<title>`
+ * @param  {Function} `<fn>`
+ * @return {Mukla}
+ * @api public
+ */
+Mukla.prototype.it =
+Mukla.prototype.test =
+Mukla.prototype.then = function then(title, fn) {
+  if (!fn) {
+    errs.error('`.then/.be` method at least 2 arguments - string and function');
+  }
+
+  if (!this.current) {
+    //todo this.current
+  }
+
+  this.store.push(test);
+  return this;
+};
+
+/**
+ * > Add test to the suite
+ *
+ * @param  {String}   `<title>`
+ * @param  {Function} `<fn>`
+ * @return {Mukla}
+ * @api public
+ */
+Mukla.prototype.be = function be(title, fn) {
+  this.then('be ' + title, fn);
+  return this;
 }
--- test.js
+++ test.js
@@ -7,33 +7,184 @@
 
 'use strict';
 
-var mukla = require('./index');
-var hybridify = require('hybridify');
-var got = require('got');
-var hybridGot = hybridify(got.get);
+var mukla = require('./index')
 
-var arr = [1, 2, {four: 'five'}]
-var abc = [1, 2, {four: 'five'}]
+// possible:
+// mukla().equal(title, actual, expected)
+// mukla().then(title, fn)
+// mukla().should(title, fn)
+// mukla().should(title).then(title, fn)
+// mukla().should(title).then(title, fn).then(title, fn)
+// mukla().should(title).equal(title, actual, expected)
 
-var obj = {one: 111, two: [1,2,3]}
-var met = {one: 111, two: [1,2,3]}
+// not possible:
+//
+// cuz', `.then` requires to have `fn`, so chain assertion methods after `.then` are not reasonable
+// if you want assertions open new `.should` or just put them on `.then`s `fn` function
+//
+// mukla().then(title).equal(title, actual, expected)
+//
+// cuz', if you want to create `next test/group/nesting` just use `.should`
+// mukla().should(title, fn).then(title, fn)
 
-mukla('some title for test', function() {
-  mukla('should one + one be strict equal to 2').strictEqual(1 + 1, 2);
-  mukla('should arr deepEqual to abc').deepEqual(arr, abc);
-  mukla('some nested testing', function() {
-    var p = mukla('fake data').ok(true);
-    mukla('should mukla(str) methods returns promise').strictEqual(typeof  p.then, 'function')
-    mukla('should two + two be strict equal to 4').strictEqual(2 + 2, 4);
-    mukla('should `obj` deepEqual to `met`').deepEqual(obj, met);
-  })
-  mukla('should `obj` be typeof object').strictEqual(typeof obj, 'object');
+// scenario #1, should count 1 test?
+// should show assertion titles? maybe configurable with opts?
+// then `.then` will be as `.describe` in mocha
+// for grouping/nesting.
+mukla('package-name:', opts)
+.then('should pass when', function(context) {
+  mukla.equal('given string is `one two`', context.str, 'one two');
+  mukla.equal('given is typeof string', typeof context.str, 'string');
 })
+.run(context); // object, array or string? in this is case object
 
-// or just asserts
-var str = 'some long string'
-mukla('should `str` be typeof of string').strictEqual(typeof str, 'string');
-mukla('should be truthy value').ok(str);
+// scenario #2, should count 1 or 3 test(s)?
+// should show assertion titles? maybe configurable with opts?
+// then `.should` will be as `.describe` in mocha
+// for grouping/nesting.
+mukla('package-yeah:', opts)
+.should('pass when', function(contextString) {
+  mukla.equal('given string is `one two`', contextString, 'one two');
+  mukla.equal('given is typeof string', typeof contextString, 'string');
+  mukla.equal('given be with length 7', contextString.length, 7);
+})
+.run('one two');
+
+// scenario #3, how much should be the tests count, i think two?
+mukla('package-beta:', opts)
+.should('pass when')
+.then('string given', function(ctx) {
+  mukla.equal('string is `one two`', ctx.str, 'one two');
+  mukla.equal('is typeof string', typeof ctx.str, 'string');
+  mukla.equal('be with length 7', ctx.str.length, 7);
+})
+.then('array given', function(ctx) {
+  mukla.deepEqual('array is `one two`', ctx.arr, [1,2,3]);
+  mukla.equal('is typeof array', ctx.arr instanceof Array, true);
+  mukla.equal('be with length 3', ctx.arr.length, 3);
+})
+.run({
+  str: 'one two',
+  arr: [1,2,3]
+});
+
+// scenario #4, should count 3 tests!
+// `.should` is just for nesting, like `.describe` in mocha
+// for grouping
+mukla('package-gama:', opts)
+.should('pass when')
+.equal('given string be `one two`', str, 'one two')
+.equal('given be typeof string', typeof str, 'string')
+.equal('given be with length 7', str.length, 7)
+.run()
+
+// scenario #5, should count 3 tests?
+mukla('package-two:', opts)
+.equal('should given string be `one two`', str, 'one two')
+.equal('should given be typeof string', typeof str, 'string')
+.equal('should given be with length 7', str.length, 7)
+.run()
+
+
+
+
+
+
+
+mukla('anonymize-ip1:', {files: files, reporter: reporter})
+.should('given `129.89.5.243`', 2)
+.then('be awesome and', function() {
+  var actual = anonymizeIp('129.89.5.243');
+  var expected = '129.89.x.xxx';
+
+  mukla.equal('equal to `129.89.x.xxx`', actual, expected);
+  mukla.equal('typeof string', typeof actual, 'string');
+})
+.run();
+
+
+mukla('anonymize-ip2:', {})
+.should('one plus one').equal('be equal to two', 1+1,2)
+.should('two plus two').equal('be equal to four', 2+2,4)
+.run();
+
+mukla('anonymize-ip3:', {})
+.should('these things', 3).be('be equal', function() {
+  mukla.equal('two arrays', [1,2,3], [1,2,3])
+  mukla.equal('two objects', {one: 123}, {one: 123})
+  mukla.equal('two strings', '[1,2,3]', '[1,2,3]')
+})
+.should('one plus one', 1).equal('be equal to two', 1+1,2)
+.should('two plus two', 2).equal('be equal to four', 2+2,4)
+.run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var hybridify = require('hybridify');
+// var got = require('got');
+// var hybridGot = hybridify(got.get);
+
+// var arr = [1, 2, {four: 'five'}]
+// var abc = [1, 2, {four: 'five'}]
+
+// var obj = {one: 111, two: [1,2,3]}
+// var met = {one: 111, two: [1,2,3]}
+
+// mukla('some title for test', function() {
+//   mukla('should one + one be strict equal to 2').strictEqual(1 + 1, 2);
+//   mukla('should arr deepEqual to abc').deepEqual(arr, abc);
+//   mukla('some nested testing', function() {
+//     var p = mukla('fake data').ok(true);
+//     mukla('should mukla(str) methods returns promise').strictEqual(typeof  p.then, 'function')
+//     mukla('should two + two be strict equal to 4').strictEqual(2 + 2, 4);
+//     mukla('should `obj` deepEqual to `met`').deepEqual(obj, met);
+//   })
+//   mukla('should `obj` be typeof object').strictEqual(typeof obj, 'object');
+// })
+
+// // or just asserts
+// var str = 'some long string'
+// mukla('should `str` be typeof of string').strictEqual(typeof str, 'string');
+// mukla('should be truthy value').ok(str);
 
 
 // mukla('should throw', function() {
